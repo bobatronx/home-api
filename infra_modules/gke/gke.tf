fbb0b9f5d4046a568ed6e05687065bc421f8d7d6
James Cefaratti <jcefaratti@gmail.com>
2021-08-30 07:08:37 -0400

    Add .out files to git ignore Formatting Configure gke with private cluster and master authorized cidr

--- infra_modules/gke/gke.tf
+++ infra_modules/gke/gke.tf
@@ -1,7 +1,19 @@
 # GKE cluster
 resource "google_container_cluster" "primary" {
-  name     = "${data.google_project.home_api.project_id}-gke"
-  location = data.google_client_config.current.region
-  
   enable_autopilot = true
+  name             = "${data.google_project.home_api.project_id}-gke"
+  location         = data.google_client_config.current.region
+
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "75.135.104.135/32"
+      display_name = "kubectl-client"
+    }
+  }
+
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false
+    master_ipv4_cidr_block  = "172.168.0.0/28"
+  }
 }
