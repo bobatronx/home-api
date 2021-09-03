0f1d59a87843aa5b3af0c527fecbdc3fb75676d4
James Cefaratti <jcefaratti@gmail.com>
2021-09-02 21:55:40 -0400

    Add nat router

--- infra_modules/gke/gke.tf
+++ infra_modules/gke/gke.tf
@@ -17,3 +17,22 @@ resource "google_container_cluster" "primary" {
     master_ipv4_cidr_block  = "172.168.0.0/28"
   }
 }
+
+resource "google_compute_router" "router" {
+  name    = "gke-router"
+  region  = data.google_client_config.current.region
+  network = google_container_cluster.primary.network
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "gke-router-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
